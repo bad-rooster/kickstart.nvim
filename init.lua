5ae4de1f2b1e4ddbcf8e92a5a609c96860492728
Adhi Nugroho <nugrohoay@protonmail.com>
2024-11-27 18:19:53 +0000

    Installing Emmet LSP

--- init.lua
+++ init.lua
@@ -587,6 +587,37 @@ require('lazy').setup({
           end
         end,
       })
+      vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = 'css,eruby,html,htmldjango,javascriptreact,less,pug,sass,scss,typescriptreact',
+        callback = function()
+          vim.lsp.start {
+            cmd = { 'emmet-language-server', '--stdio' },
+            root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
+            -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+            -- **Note:** only the options listed in the table are supported.
+            init_options = {
+              ---@type table<string, string>
+              includeLanguages = {},
+              --- @type string[]
+              excludeLanguages = {},
+              --- @type string[]
+              extensionsPath = {},
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+              preferences = {},
+              --- @type boolean Defaults to `true`
+              showAbbreviationSuggestions = true,
+              --- @type "always" | "never" Defaults to `"always"`
+              showExpandedAbbreviation = 'always',
+              --- @type boolean Defaults to `false`
+              showSuggestionsAsSnippets = false,
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+              syntaxProfiles = {},
+              --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+              variables = {},
+            },
+          }
+        end,
+      })
 
       -- Change diagnostic symbols in the sign column (gutter)
       if vim.g.have_nerd_font then
@@ -659,6 +690,7 @@ require('lazy').setup({
         'clang-format',
         'isort',
         'black', -- Used to format Lua code
+        'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -716,7 +748,7 @@ require('lazy').setup({
         c = { 'clang-format' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
