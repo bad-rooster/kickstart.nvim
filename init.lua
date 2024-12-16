93bed3986d34c43ad3898297d229a32121ddecee
Adhi Nugroho <nugrohoay@protonmail.com>
2024-12-16 23:18:31 +0000

    Nvim config update

--- init.lua
+++ init.lua
@@ -691,6 +691,8 @@ require('lazy').setup({
         'isort',
         'black', -- Used to format Lua code
         'prettier',
+        'prettierd',
+        'eslint_d',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -748,7 +750,7 @@ require('lazy').setup({
         c = { 'clang-format' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true, 'eslint_d' },
       },
     },
   },
